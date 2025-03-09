fe9901530253bcf65996753bbcb86adaac7bb690
Albert Tang <tangalbert919@yahoo.com>
2025-03-09 11:46:24 -0700

    billie: Build fingerprint HAL from source
    Saalim: Remove "egis" part so AOSP fingerprint HAL can actually load it.
    
    Change-Id: I4002bbca52e21aa780b967f79535eebb335853ea
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.billie
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Framework detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
