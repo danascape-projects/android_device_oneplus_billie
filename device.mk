e982c5bfb7d2d724aaaa7cc0207707e1be64ac8f
Saalim Quadri <danascape@gmail.com>
2026-02-12 16:19:10 +0530

    billie: Convert Aperture overlay to RRO
    Change-Id: I98e0fd2c431e641ee7dfa10311648537116124fe
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -42,6 +42,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureOverlayDevice \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
