9dbae15a516da2c09425ce5b1339fa39d9bae089
Saalim Quadri <danascape@gmail.com>
2026-02-12 18:54:19 +0530

    billie: fastch: Invert the toggle
    - match kernel instead
    
    Change-Id: I0552e8a4f322062ff3e0aedcf5679e61d3ad6f7e
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- fastcharge/FastCharge.cpp
+++ fastcharge/FastCharge.cpp
@@ -82,11 +82,11 @@ FastCharge::FastCharge() {
 }
 
 Return<bool> FastCharge::isEnabled() {
-    return get(FASTCHARGE_PATH, 0) < 1;
+    return get(FASTCHARGE_PATH, 1) > 0;
 }
 
 Return<bool> FastCharge::setEnabled(bool enable) {
-    set(FASTCHARGE_PATH, enable ? 0 : 1);
+    set(FASTCHARGE_PATH, enable ? 1 : 0);
 
     bool enabled = isEnabled();
     property_set(kFastChargingProp, enabled ? "true" : "false");
